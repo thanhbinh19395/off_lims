55fd406fbc1289d099b4155bf71f64305fe096db
Nguyen Ho Phuong <phuongnh.fuky@gmail.com>
2017-02-21 16:22:04 +0700

    fix bug Update password

--- LiMS/src/main/java/hcmue/gst/off/controllers/User/RequestPurchaseController.java
+++ LiMS/src/main/java/hcmue/gst/off/controllers/User/RequestPurchaseController.java
@@ -5,6 +5,7 @@ import hcmue.gst.off.services.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,13 +14,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by Hung Thinh on 19/02/2017.
  */
 @Controller
+@RequestMapping("/User")
 public class RequestPurchaseController extends UserBaseController{
 
     @Autowired
     private RequestService requestService;
 
     @RequestMapping(value = "/RequestPurchase", method = RequestMethod.POST)
-    public String RequestPurchase(@ModelAttribute("request") Request rq) {
+    public String RequestPurchase(@ModelAttribute("request") Request rq, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/RequestPurchase?error";
+        }
         requestService.save(rq);
         return "redirect:/RequestPurchase?success";
     }
